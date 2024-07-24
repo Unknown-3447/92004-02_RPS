45c142658a5d9b14b66d569d28d2e3cec02e8910
Unknown-3447 <Kieranborthwick@mmc.school.nz>
2024-07-25 09:29:19 +1200

    Important for Assessment code

---
+++ R_00_rounds.py
@@ -0,0 +1,72 @@
+# checks for an integer more than 0 (allows <enter>)
+def int_check(question):
+    while True:
+
+        error = "Please enter an integer that is 1 or more."
+
+        to_check = input(question)
+
+        # check for infinite mode
+        if to_check == "":
+            return "infinite"
+
+        try:
+            response = int(to_check)
+
+            # checks that the number is more than / equal to 1
+            if response < 1:
+                print(error)
+            else:
+                return response
+
+        except ValueError:
+            print(error)
+
+
+# Main routine Starts here
+
+# Initialise game variable
+mode = "regular"
+rounds_played = 0
+
+print("Rock / Paper / Scissors Game")
+print()
+
+# Instructions
+
+# Ask user for number of rounds / infinite mode
+num_rounds = int_check("How many rounds would you like? Push <enter> for infinite mode: ")
+
+if num_rounds == "infinite":
+    mode = "infinite"
+    num_rounds = 5
+
+# Game loop starts here
+while rounds_played < num_rounds:
+
+    # Rounds headings (based on mode)
+    if mode == "infinite":
+        rounds_heading = f"\n Round {rounds_played + 1} (Infinite Mode)"
+    else:
+        rounds_heading = f"\n Round {rounds_played + 1} of {num_rounds} "
+
+    print(rounds_heading)
+    print()
+
+    # get user choice
+    user_choice = input("Choose: ")
+
+    # If user choice is the exit code, break the loop
+    if user_choice == "xxx":
+        break
+
+    rounds_played += 1
+
+    # if users are in infinite mode, increase number of rounds!
+    if mode == "infinite":
+        num_rounds += 1
+
+
+# Game loop ends here
+
+# Game history / Statistics area
